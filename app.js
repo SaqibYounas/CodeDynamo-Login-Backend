71934d17623c8e922f0328a8b017266f28de100d
Saqib Younas <muhammadsaqibyounas11@gmail.com>
2025-11-04 04:51:40 +0500

    production

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(passport.initialize());
 const isProduction = process.env.NODE_ENV === "production";
 
 const allowedOrigins = isProduction
-  ? ["https://code-dynamo-login-backend.vercel.app"]
+  ? ["https://code-dynamo-frontend.vercel.app"]
   : ["http://localhost:5173"];
 
 app.use(cors({
